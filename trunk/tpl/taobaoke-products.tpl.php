9b22f52dee0ce25ca1d9f3a311e0d298b31e8920
WyattFang <WyattFang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-17 01:40:38 +0000

    增加了搜索功能。
    git-svn-id: https://plugins.svn.wordpress.org/taobaoke-plugin-for-wordpress@155561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/tpl/taobaoke-products.tpl.php
+++ trunk/tpl/taobaoke-products.tpl.php
@@ -1,4 +1,21 @@
 <link rel='stylesheet' href='<?php _e_(taobaoke_css_path() . 'style.css'); ?>' type='text/css' media='all' />
+<table width="98%">
+  <tr>
+    <td align="center">
+      <form method="get">
+        <input type="text" size="40" name="taobaoke_item_search" id="taobaoke_item_search" />
+        <input class="button" style="font-weight:bold" type="submit"  value="搜索一下，你就知道" />
+        <span style="font-size:10px;font-style:italic;">留空搜索全部...</span>
+        <?php if (count($query_string)):?>
+          <?php foreach($query_string as $key => $value):
+                echo "<input type='hidden' name='{$key}' value='{$value}' />";
+          ?>
+          <?php endforeach;?>
+        <?php endif;?>
+      </form>
+    </td>
+  </tr>
+</table>
 <?php if ($has_cats):?>
     <h2>选择您想要推广的分类</h2>
     <?php
