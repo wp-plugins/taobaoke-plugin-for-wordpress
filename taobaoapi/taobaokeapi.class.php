5ba5b916f5838a9bce734df790d6bf6bef34262c
WyattFang <WyattFang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-19 12:53:58 +0000

    增加了推广店铺的功能。
    git-svn-id: https://plugins.svn.wordpress.org/taobaoke-plugin-for-wordpress/trunk@156200 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- taobaoapi/taobaokeapi.class.php
+++ taobaoapi/taobaokeapi.class.php
@@ -13,5 +13,13 @@ class TaobaokeApi extends TaobaoApi {
         //The structure of the $entity_result is: $entity_result = array('taobaokeItems' => array(0=>obj, 1=>obj))
         return $this->sendRequest($request, 'TaobaokeApi:convertItems', 'taobao.taobaoke.items.convert');
     }
+
+    public function getShop(TaobaokeShopGetRequest $request) {
+        return $this->sendRequest($request, 'TaobaokeApi:getShop', 'taobao.shop.get');
+    }
+
+    public function convertShop(TaobaokeShopConvertRequest $request) {
+        return $this->sendRequest($request, 'TaobaokeApi:convertShop', 'taobao.taobaoke.shops.convert');
+    }
 }
 ?>
