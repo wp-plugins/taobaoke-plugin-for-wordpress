f3d6e01f77ab40e306d6283d306bdcaaefb57acc
WyattFang <WyattFang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-19 12:55:30 +0000

    增加了推广店铺的功能。
    git-svn-id: https://plugins.svn.wordpress.org/taobaoke-plugin-for-wordpress@156203 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/tpl/taobaoke-shop.tpl.php
@@ -0,0 +1,42 @@
+<table width="650px">
+  <?php if (!empty($message)): ?>
+    <tr>
+      <td colspan=2 style="font-weight:bold" align="center">
+        <?php if (!empty($message)) {_e_($message);} ?>
+      </td>
+    </tr>
+  <?php endif;?>
+  <?php if (!empty($shop)): ?>
+    <tr>
+      <td width="80%">
+        <table width="100%">
+          <tr align="left">
+            <td rowspan="3"><a href="<?php echo $converted_shop['click_url']; ?>" target="_blank"><img style="width:100px;height:100px" src="http://logo.taobao.com/shop-logo/<?php echo $shop['pic_path'];?>"</a></td>
+            <td>
+              掌柜: <a style="font-weight:bold" href="<?php echo $converted_shop['click_url']; ?>" target="_blank"><?php echo $shop['nick'];?></a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              店铺名: <a style="font-weight:bold" href="<?php echo $converted_shop['click_url']; ?>" target="_blank"><?php echo $shop['title'];?></a>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              平均佣金比率: <a style="font-weight:bold" href="<?php echo $converted_shop['click_url']; ?>" target="_blank"><?php echo ((float)$converted_shop['shop_commission_rate'] * 100 ) . '%';?></a>
+            </td>
+          </tr>
+        </table>
+      </td>
+      <td>
+        <a class="button-primary" href="<?php echo $shop_fav_url ?>">加入推广列表</a><br /><br />
+        <a class="button-primary" href="<?php echo $shop_promote_url;?>">侧边栏推荐</a>
+      </td>
+    </tr>
+    <tr>
+      <td colspan=2 style="font-style:italic;text-align:left">
+        听听店长怎么说：>>> <br /><?php _e_($shop['bulletin']); ?>
+      </td>
+    </tr>
+  <?php endif;?>
+</table>
