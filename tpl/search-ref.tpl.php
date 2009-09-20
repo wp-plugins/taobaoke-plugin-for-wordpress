c6e23a6714dccb7b4560bfef7767d3235a2c3c64
WyattFang <WyattFang@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-09-20 07:38:28 +0000

    增加了搜索功能
    git-svn-id: https://plugins.svn.wordpress.org/taobaoke-plugin-for-wordpress/trunk@156393 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tpl/search-ref.tpl.php
@@ -0,0 +1,32 @@
+<?php if (!empty($message)):?>
+<h3><?php echo $message;?></h3>
+<?php endif;?>
+<form method="post">
+  <span style="font-size:10px;font-style:italic">输入文字&nbsp;&nbsp;</span><input type="text" size=20 name="taobaoke_search_keyword" />
+  <span style="font-size:10px;font-style:italic">并加入淘宝客推广链接, 然后以别名</span>
+  <input type="text" size=20 name="taobaoke_search_alias" />
+  <input type="submit" value="插入到文章中" class="primary-button" />
+</form>
+<ul>
+  <li>
+    <strong>Q: 为什么要在文章中加入淘宝搜索的链接?</strong><br />
+    <div>
+      A: 通过在文章中插入链接, 这是引导用户点击最好的办法. 例如, 当你在发表博客的时候, 你需要使用某个次<strong>"电话卡"</strong>, 那么你就可以给<strong>"电话卡"</strong>加入一个链接,
+      把这个链接指向淘宝的搜索页面, 这样当有用户点击了<strong>"电话卡"</strong>这个关键字,那么就会引导用户到淘宝搜索<strong>"电话卡"</strong>, 如果用户发生了购买行为, 那么你就可以收到
+      淘宝的佣金了.<br />
+      所以, 在你写博客的时候, 尽量多插些关键字进去吧! 站长不容易, 站长也要生活啊~~
+    </div>
+  </li>
+  <li>
+    <strong>Q: 一个一个关键字插入是不是很繁琐,有没有导入关键字功能啊?</strong><br />
+    <div>
+      A: 目前只开发了手动插入 关键字+链接 的功能, 后面会根据插件的使用情况, 增加导入关键字的功能, 当你在写博客的时候, 会自动把你导入的关键字列表在文章中出现的地方加上淘宝客的链接. 敬请期待和支持 <a href="http://blog.gotall.net/" target="_blank">大房</a> 啊~~
+    </div>
+  </li>
+  <li>
+    <strong>Q: 还有其他功能吗?</strong><br />
+    <div style="font-weight:bold;font-size:15px;">
+      A: 如果对于扩展淘宝客插件，你有好的需求和想法，记得告知<a href="http://blog.gotall.net/" target="_blank">大房</a>，我们一起研究~~
+    </div>
+  </li>
+</ul>
